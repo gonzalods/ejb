5e71d723caeb12a6ed5dcc71f3bf776bae83f666
0003299 <gms@mail>
2016-09-14 12:35:55 +0200

    cdi - web simple

---
+++ Ejemplos Web/src/main/java/com/viewnextfor/ejb/servlet/SaludoServlet.java
@@ -0,0 +1,42 @@
+package com.viewnextfor.ejb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SaludoServlet
+ */
+@WebServlet("/saludo")
+public class SaludoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		writer.append("<html>")
+			.append("<head>")
+			.append("<title>Saludo</title>")
+			.append("</head>")
+			.append("<body>")
+			.append("<h2>Saludos desde nuestra aplicación</h2>")
+			.append("</body>")
+			.append("</html>").flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
