18519f70fbe034b15732717dc34888d1295be68d
0003299 <gms@mail>
2016-09-15 08:26:55 +0200

    cdi - web. Método productor

--- Ejemplos Web/src/main/java/com/viewnextfor/servicio/ServicioSaludoFactory.java
+++ Ejemplos Web/src/main/java/com/viewnextfor/servicio/ServicioSaludoFactory.java
@@ -1,14 +1,12 @@
 package com.viewnextfor.servicio;
 
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 
 import com.viewnextfor.bean.SaludoBean;
 
 public class ServicioSaludoFactory {
 
 	@Produces
-	@Inject
 	public ServicioSaludo crearServicioSaludo(SaludoBean saludo){
 		return new SaludoNewApi("Bienvenido a nuestro programa CDI", saludo);
 	}
